2421059e7b9e8dc9f1b6c82f71c3a9323046f21c
haziqshahard <146861553+haziqshahard@users.noreply.github.com>
2024-08-26 12:49:19 +0300

    Change light/dark mode
    Need to adjust all the colours as a whole

--- app.py
+++ app.py
@@ -1,7 +1,40 @@
 import tkinter as tk
 from tkinter import filedialog, Text
 import os
+from customtkinter import *
 
-root=tk.Tk()
+mode = "dark"
 
-root.mainloop()
+def doNothing():
+    print("ok ok I wont")
+
+def switchDarkmode():
+    global mode
+    if mode == "dark":
+        set_appearance_mode("light")
+        mode = "light"
+    else:
+        set_appearance_mode("dark")
+        mode = "dark"
+
+app=CTk()
+app.geometry("500x400")
+
+btn = CTkButton(master=app, text="Click Me", corner_radius=32,command=doNothing)
+btn.place(relx=0.5, rely=0.5, anchor="center")
+
+menu = tk.Menu(app)
+app.config(menu = menu)
+
+subMenu = tk.Menu(menu)
+menu.add_cascade(label="File",menu=subMenu)
+subMenu.add_command(label="Do Nothing",command=doNothing)
+subMenu.add_separator()
+subMenu.add_command(label="Exit",command=doNothing)
+subMenu.add_command(label="Light/Dark",command=switchDarkmode)
+
+editMenu=tk.Menu(menu)
+menu.add_cascade(label="Edit",menu=editMenu)
+editMenu.add_command(label="Undo",command=doNothing)
+
+app.mainloop()
